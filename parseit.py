064d0bd8025b3f739d14df5d191520531592927f
Marnix Klooster <marnix.klooster@gmail.com>
2021-10-20 20:39:37 +0200

    Added selftest script to run pytest tests

--- parseit.py
+++ parseit.py
@@ -13,7 +13,7 @@ from type_docopt import docopt
 
 import mmlib
 from collections import defaultdict
-from findsubst import *
+import findsubst
 from itertools import product
 import json
 
@@ -52,7 +52,7 @@ def find_proofs(expression, asss):
             "short-cut assumption that a syntax $a has no $e expected to hold for assertion %s: %s" % (name, assertion))
         assertion_vars = [v for l, (t, v) in assertion.types]
         assert len(assertion.types) > 0 or assertion_vars == []
-        for substitution in find_substitutions(assertion_vars, assertion.conclusion, expression):
+        for substitution in findsubst.find_substitutions(assertion_vars, assertion.conclusion, expression):
             # we found a possible proof step
             #print('s', substitution)
             subproofss = (
@@ -69,54 +69,45 @@ def find_proofs(expression, asss):
     #print('<looked at :', expression)
     return
 
-from type_docopt import docopt
-arguments = docopt()
+if __name__ == '__main__':
+    from type_docopt import docopt
+    arguments = docopt()
 
-with open(arguments['--input'], 'rb') as f:
-    setmm = f.read()
-    assertions_by_kind = defaultdict(dict)
-    for nr, (name, typ, assertion) in enumerate(mmlib.mm_assertions(setmm)):
-        #print(name, assertion.conclusion)
-        t = assertion.conclusion[0]
-        once = True
-        while once:
-            once = False
-            if t == b'TOP':
-                # don't parse
-                continue
-            if typ == ord(b'f'):
-                # don't parse $f statements
-                continue
-            if typ == ord(b'a') and chr(t[0]).upper() != chr(t[0]):
-                # don't parse $a statements whose typecode starts with lowercase letter
-                continue
-            try:
-                if nr % 1 == 0:
-                    print(nr, ':', str(name, 'ASCII'), str(
-                        b' '.join(assertion.conclusion), 'ASCII'))
-                    parse_tree = parse(assertion.conclusion,
-                                       b'TOP', assertions_by_kind)
-                if nr % 1 == 0:
-                    print(nr, ':', '-->', json.dumps(parse_tree,
-                          indent=None, default=lambda x: str(x, 'ASCII')))
-                    print()
-            except Exception:
-                print(name, assertion.conclusion)
-                import traceback
-                traceback.print_exc()
-                import sys
-                sys.exit(1)
-        if typ in (ord(b'a'), ord(b'f')):
-            assertions_by_kind[t][name] = assertion
-            if False:
-                print('using:', t, name, assertion)
-
-    # print(assertions_by_kind[b'TOP'])
-
-    EXPRESSION = list(map(
-        bytes, '|- ( [C.] Or Y -> [C.] Or { u e. ~P A | ( A \ u ) e. Y } )'.encode('ASCII').split()))
-    #EXPRESSION=list(map(bytes, '|- ps'.encode('ASCII').split()))
-    #EXPRESSION=list(map(bytes, 'wff ph'.encode('ASCII').split()))
-    # print(EXPRESSION)
-
-    #print(parse(EXPRESSION, b'TOP', assertions_by_kind))
+    with open(arguments['--input'], 'rb') as f:
+        setmm = f.read()
+        assertions_by_kind = defaultdict(dict)
+        for nr, (name, typ, assertion) in enumerate(mmlib.mm_assertions(setmm)):
+            #print(name, assertion.conclusion)
+            t = assertion.conclusion[0]
+            once = True
+            while once:
+                once = False
+                if t == b'TOP':
+                    # don't parse
+                    continue
+                if typ == ord(b'f'):
+                    # don't parse $f statements
+                    continue
+                if typ == ord(b'a') and chr(t[0]).upper() != chr(t[0]):
+                    # don't parse $a statements whose typecode starts with lowercase letter
+                    continue
+                try:
+                    if nr % 1 == 0:
+                        print(nr, ':', str(name, 'ASCII'), str(
+                            b' '.join(assertion.conclusion), 'ASCII'))
+                        parse_tree = parse(assertion.conclusion,
+                                           b'TOP', assertions_by_kind)
+                    if nr % 1 == 0:
+                        print(nr, ':', '-->', json.dumps(parse_tree,
+                              indent=None, default=lambda x: str(x, 'ASCII')))
+                        print()
+                except Exception:
+                    print(name, assertion.conclusion)
+                    import traceback
+                    traceback.print_exc()
+                    import sys
+                    sys.exit(1)
+            if typ in (ord(b'a'), ord(b'f')):
+                assertions_by_kind[t][name] = assertion
+                if False:
+                    print('using:', t, name, assertion)
